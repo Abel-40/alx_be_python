460dd1ad21f850ec1dcfc6ecf0fcfc3ca10783e7
Abel <addis0132@gmail.com>
2024-09-26 11:25:09 +0300

    v4 of dail-r

--- control-flow/daily_reminder.py
+++ control-flow/daily_reminder.py
@@ -1,21 +1,20 @@
-Task = input("Enter your task:")
-Priority = input("Priority (high/medium/low:)")
-Time_bound = input("Is it time-bound?(yes/no):")
+task = input("Enter your task: ")
+priority = input("Priority (high/medium/low): ")
+time_bound = input("Is it time-bound? (yes/no): ")
 
-match Priority:
-  case "high":
-    if Time_bound == "yes":
-        print(f"Reminder: '{Task}' is a high priority task that requires immediate attention today!")
-    else:
-         print(f"Note: '{Task}' is a high priority task. Consider completing it when you have free time.")
-  case "low":
-    if Time_bound == "yes":
-        print(f"Reminder: '{Task}' is a high priority task that requires immediate attention today!")
-    else:
-         print(f"Note: '{Task}' is a low priority task. Consider completing it when you have free time.")
-  case "medium":
-    if Time_bound == "yes":
-        print(f"Reminder: '{Task}' is a medium priority task that requires immediate attention today!")
-    else:
-         print(f"Note: '{Task}' is a medium priority task. Consider completing it when you have free time.")
-    
+match priority:
+    case "high":
+        if time_bound == "yes":
+            print(f"Reminder: '{task}' is a high priority task that requires immediate attention today!")
+        else:
+            print(f"Note: '{task}' is a high priority task. Consider completing it when you have free time.")
+    case "medium":
+        if time_bound == "yes":
+            print(f"Reminder: '{task}' is a medium priority task that requires immediate attention today!")
+        else:
+            print(f"Note: '{task}' is a medium priority task. Consider completing it when you have free time.")
+    case "low":
+        if time_bound == "yes":
+            print(f"Reminder: '{task}' is a low priority task that requires immediate attention today!")
+        else:
+            print(f"Note: '{task}' is a low priority task. Consider completing it when you have free time.")
