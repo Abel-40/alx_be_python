12fa7b17b3f0b420654c27c52831c08f48ffdb1d
Abel <addis0132@gmail.com>
2024-10-03 22:50:18 +0300

    arithmetic_operation v3

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -1,12 +1,13 @@
 def perform_operation(num1,num2,operation):
   if operation == 'add':
-    return num1 + num2
+    result = num1 + num2
   elif operation == 'subtract':
-    return num1 - num2
+    result = num1 - num2
   elif operation == 'multiply':
-    return num1 * num2
+   result = num1 * num2
   elif operation == 'divide':
     if num2 > 0:
-        return num1/num2
+        result = num1/num2
     else:
-        return "Cannot divide by zero"
+        result = "Cannot divide by zero"
+  return result
