5545eee2d0e5e14c99be039d0657c6c38ba64156
Abel <addis0132@gmail.com>
2024-10-04 11:04:49 +0300

    arithmetic_operations v5

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -1,13 +1,15 @@
-def perform_operation(num1,num2,operation):
-  if operation == 'add':
-    result = num1 + num2
-  elif operation == 'subtract':
-    result = num1 - num2
-  elif operation == 'multiply':
-   result = num1 * num2
-  elif operation == 'divide':
-    if num2 > 0:
-        result = num1/num2
-    else:
-        result = "Cannot divide by zero"
+def perform_operation(num1, num2, operation):
+  result = ""
+  match operation:
+    case 'add':
+      result = num1 + num2
+    case 'subtract':
+      result = num1 - num2
+    case 'multiply':
+      result = num1 * num2
+    case 'divide':
+      if num2 == 0:
+        result = "division by zero is undefined"
+      elif num2 != 0:
+        result = num1 / num2
   return result
