87e4862a57eb42935b71e3e431e29f225858332c
Abel <addis0132@gmail.com>
2024-10-04 12:02:46 +0300

    shopping_list v1

---
+++ fns_and_dsa/shopping_list_manager.py
@@ -0,0 +1,36 @@
+def display_menu():
+    print("Shopping List Manager")
+    print("1. Add Item")
+    print("2. Remove Item")
+    print("3. View List")
+    print("4. Exit")
+
+def main():
+    shopping_list = ["abel","aberham","daniel"]
+    while True:
+        display_menu()
+        choice = input("Enter your choice: ")
+
+        if choice == '1':
+            # Prompt for and add an item
+            item_name = input("Enter name of the item to add: ")
+            shopping_list.append(item_name)
+        elif choice == '2':
+            # Prompt for and remove an item
+            item_name = input("Enter the name of item to remove: ")
+            if item_name == shopping_list:
+                shopping_list.remove(item_name)
+            else:
+                print("please enter the write item from the list to remove.")
+        elif choice == '3':
+            # Display the shopping list
+            for item in shopping_list:
+                print(item)
+        elif choice == '4':
+            print("Goodbye!")
+            break
+        else:
+            print("Invalid choice. Please try again.")
+
+if __name__ == "__main__":
+    main()
