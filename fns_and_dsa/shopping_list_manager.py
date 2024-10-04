0f14012b0aa535b7b4b8882baa8c16f4cfde5685
Abel <addis0132@gmail.com>
2024-10-04 12:09:44 +0300

    shopping_list v3

--- fns_and_dsa/shopping_list_manager.py
+++ fns_and_dsa/shopping_list_manager.py
@@ -13,7 +13,7 @@ def main():
 
         if choice == '1':
             # Prompt for and add an item
-            item_name = input("Enter name of the item to add: ")
+            item_name = input("Enter the item to add: ")
             shopping_list.append(item_name)
         elif choice == '2':
             # Prompt for and remove an item
