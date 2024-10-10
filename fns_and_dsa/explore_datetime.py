3727627a7511232024d5e47ca389bd14032e8981
Abel <addis0132@gmail.com>
2024-10-10 22:48:35 +0300

    v1 of bank_account

--- fns_and_dsa/explore_datetime.py
+++ fns_and_dsa/explore_datetime.py
@@ -5,7 +5,6 @@ def display_current_datetime():
     print(f"Current date and time: {current_date.strftime('%Y-%m-%d %H:%M:%S')}")
 
 display_current_datetime()
-
 def calculate_future_date():
     current_date = datetime.now()
     number_of_days = int(input("Enter the number of days to add to the current date: "))
---
+++ programming_paradigm/bank_account.py
@@ -0,0 +1,24 @@
+class BankAccount:
+
+  def __init__(self,inital_balance = 0):
+    self.__account_balance = inital_balance
+
+  def deposit(self,amount):
+   if self.__account_balance > 0:
+    self.__account_balance += amount
+    return self.__account_balance
+   else:
+    return print("please enter non negative number")
+  
+  def withdraw(self, amount):
+    if amount > self.__account_balance:
+        print("Insufficient funds.")
+        return False
+    self.__account_balance -= amount
+    return True
+
+
+  def display_balance(self):
+    print(f"Current Balance: {self.__account_balance}")
+
+  
