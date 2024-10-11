c5ff8f67401be19bdd7a86079d8589477c5593bf
Abel <addis0132@gmail.com>
2024-10-11 23:41:27 +0300

    v1 of book_class

---
+++ oop/book_class.py
@@ -0,0 +1,19 @@
+class Book:
+  def __init__(self, title,author,year):
+    self.title = str(title)
+    self.author = str(author)
+    self.year = int(year)
+
+  def __del__(self):
+    print(f"Deleting {self.title}")
+  
+  def __str__(self):
+    return f"{self.title} by {self.author}, published in {self.year}"
+  
+  def __repr__(self):
+    return f"Book('{self.title}', '{self.author}', {self.year})"
+
+my_book = Book("1984", "George Orwell", 1949)
+
+    # Demonstrating the __str__ method
+print(repr(my_book)) 
