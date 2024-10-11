cc3ec91f10573196748d5cc26132012b39c7725c
Abel <addis0132@gmail.com>
2024-10-11 12:47:59 +0300

    v4 of test_simple_calc

--- programming_paradigm/test_simple_calculator.py
+++ programming_paradigm/test_simple_calculator.py
@@ -12,10 +12,10 @@ class TestClass(unittest.TestCase):
   def test_subtraction(self):
     self.assertEqual(self.calc.subtract(5,3),2)
 
-  def test_multiply(self):
+  def test_multiplication(self):
     self.assertEqual(self.calc.multiply(5,3),15)
   
-  def test_divide(self):
+  def test_division(self):
     self.assertEqual(self.calc.divide(4,2),2)
   
   def test_division_by_zero(self):
